f475fa9c687e269ad9a3277ff1364da50b1ad2e3
Terriblefire <sleary@vavi.co.uk>
2026-05-03 15:45:56 +0100

    Sync __init__.__version__ to 0.4.3

--- src/tflab/__init__.py
+++ src/tflab/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 from .hp1660 import (HP1660, SocketTransport, VisaTransport,
                      FormatConfig, TriggerConfig, TriggerTerm, TriggerLevel,
